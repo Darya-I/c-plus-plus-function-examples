300e6af9e6d38d566cf4b231f21775f121ebf84c
Дарья Григорьевнаа Ильченко <human.darrrya@gmail.com>
2023-12-03 01:11:38 +0300

    Removed unnecessary files

--- Fuction_convert.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef Function_convert
-#define Function_convert
-
-double ConvertToUSD(double rub);
-
-#endif // !Function_convert
--- Function_y.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#ifndef Function_y
-#define Function_y
-
-double Calculate_y(double x);
-
-#endif
